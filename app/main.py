e0623689c058c196e37e74983a9b4bfa2b9cf42e
shoya4000 <mikevoorthuyzen@hotmail.com>
2017-03-04 16:43:00 -0800

    Cut-off
    Moves to cut-off snake running beside our snake

--- app/main.py
+++ app/main.py
@@ -221,6 +221,31 @@ def move():
                         taunt = "im in ur base stealin ur foodz"
                         break
 
+        current_direction = direction(snek['coords'][1], snek['coords'][0])
+
+        for enemy in data['snakes']:
+            if current_direction == 'left' || current_direction == 'right':
+                if (len(enemy['coords']) < len(snek['coords'])-1):
+                    if enemy['coords'][0] == snek['coords'][1][1]+1:
+                        path = [(snek['coords'][0][0],snek['coords'][0][1]+1)]
+                    elif enemy['coords'][0] == snek['coords'][1][1]-1:
+                        path = [(snek['coords'][0][0],snek['coords'][0][1]-1)]
+                else:
+                    if enemy['coords'][0] == snek['coords'][2][1]+1:
+                        path = [(snek['coords'][0][0],snek['coords'][0][1]+1)]
+                    elif enemy['coords'][0] == snek['coords'][2][1]-1:
+                        path = [(snek['coords'][0][0],snek['coords'][0][1]-1)]
+            elif current_direction == 'up' || current_direction == 'down':
+                if (len(enemy['coords']) < len(snek['coords'])-1):
+                    if enemy['coords'][0] == snek['coords'][1][0]+1:
+                        path = [(snek['coords'][0][0]+1,snek['coords'][0][1])] 
+                    elif enemy['coords'][0] == snek['coords'][1][0]-1:
+                        path = [(snek['coords'][0][0]-1,snek['coords'][0][1])]
+                else:
+                    if enemy['coords'][0] == snek['coords'][2][0]+1:
+                        path = [(snek['coords'][0][0]+1,snek['coords'][0][1])]
+                    elif enemy['coords'][0] == snek['coords'][2][0]-1:
+                        path = [(snek['coords'][0][0]-1,snek['coords'][0][1])]
 
         # Update snek
         if path_length < snek_length:
